69b15cfd9a994ca74b67102776f705a71899be8a
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2022-02-19 21:11:41 +0300

    Replaced points on commas in all shortened numbers

--- src/helpers/shorten-number.js
+++ src/helpers/shorten-number.js
@@ -1,4 +1,6 @@
 const shortenNumber = (number) => {
+    const replacePointOnComma = (str) => str.toString().replace(".", ",");
+
     const roundOneDecimalDigit = (number) => Math.round(number * 10) / 10;
 
     if (number / 1_000.0 < 1) {
@@ -6,10 +8,10 @@ const shortenNumber = (number) => {
     }
 
     if (number / 1_000_000.0 < 1) {
-        return `${roundOneDecimalDigit(number / 1_000.0)}K`;
+        return replacePointOnComma(`${roundOneDecimalDigit(number / 1_000.0)}K`);
     }
 
-    return `${roundOneDecimalDigit(number / 1_000_000.0)}M`;
+    return replacePointOnComma(`${roundOneDecimalDigit(number / 1_000_000.0)}M`);
 };
 
 export default shortenNumber;
